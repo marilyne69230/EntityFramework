c8254786165a71a648340048b80cfdbbec18eb44
Marilyne Naili <marilyne.lydia@gmail.com>
2024-02-13 16:38:23 +0100

    first

---
+++ PublisherConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using PublisherData;
+using PublisherDomain;
+
+/*using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}*/
+
+// AJOUTER UN AUTEUR
+/*AddAuthor("Anthony", "Caron");
+AddAuthor("Magalie", "Sassi");
+AddAuthor("Merzak", "Naili");
+AddAuthor("Marilyne", "Naili");
+AddAuthor("Michele", "Charles");
+AddAuthor("Aylan", "Dupont");*/
+void AddAuthor(string firstName, string lastName)
+{
+    var author = new Author { FirstName = firstName, LastName = lastName };
+    // firstname et lastname sere exécuter ç la fin de la requête
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+// RECUPERER LA LISTE DES AUTEURS
+GetAuthors();
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.OrderBy(a => a.LastName).ThenBy(a => a.FirstName).ToList();
+  
+    foreach (var item in authors.Skip(0).Take(3))
+    {
+        Console.WriteLine($"AUTEUR {item.Id} : {item.LastName} {item.FirstName} ");
+    }
+}
---
+++ PublisherData/PubContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PublisherDB;")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+
+    }
+}
